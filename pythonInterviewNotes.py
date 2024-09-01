e3406a936ef84b177db9c787c1853552fd98a069
Sadad Khan <sadadk673@gmail.com>
2024-08-31 18:34:44 -0700

    finsihed Pythong for interviews vid

--- pythonInterviewNotes.py
+++ pythonInterviewNotes.py
@@ -161,7 +161,7 @@ for n in nums:
 
 # with index and value
 for i, n in enumerate(nums):
-    print(i, n)
+    print("enumerate ", i, n)
 
 # Loop through multiple arrays simultaneously
 # with unpacking
@@ -337,3 +337,95 @@ print((1, 2) in mySet)
 
 # Heaps
 import heapq
+
+# under the hood heaps are arrays
+minHeap = []
+heapq.heappush(minHeap, 3)
+heapq.heappush(minHeap, 2)
+heapq.heappush(minHeap, 4)
+heapq.heappush(minHeap, 10)
+
+# min is always at index 0
+print(minHeap[0])
+
+while len(minHeap):
+    print(heapq.heappop(minHeap))
+
+# No max heaps by default, work around is to use
+# min heap and multiply by -1 when push & pop.
+maxHeap = []
+heapq.heappush(maxHeap, -3)
+heapq.heappush(maxHeap, -2)
+heapq.heappush(maxHeap, -4)
+heapq.heappush(maxHeap, -10)
+
+# max is always at index 0
+print(-1 * maxHeap[0])
+
+# multiplying by -1 when pushing and then agian for popping negates it and prints out in 
+# greatest to smallest
+while len(maxHeap):
+    print(-1 * heapq.heappop(maxHeap))
+
+# build heap from initial values
+arr = [2, 1, 8, 4, 5]
+heapq.heapify(arr)
+while arr:
+    print(heapq.heappop(arr))
+
+# Functions
+def myFunc(n, m):
+    return n * m
+
+print(myFunc(3, 4))
+
+# Nested functions have access to outer variables
+def outer(a, b):
+    c = "c"
+
+    def inner():
+        return a + b + c
+    return inner()
+
+print(outer("a", "b"))
+
+# Can modify objects but not reassign unless using nonlocal keyword
+def double(arr, val):
+    def helper():
+        # Modifying array works
+        for i, n in enumerate(arr):
+            arr[i] *= 2
+        
+        # will only modify val in the helper scope
+        # val *= 2
+
+        # this will modify val outside helper scope
+        nonlocal val
+        val *= 2
+    helper()
+    print(arr, val)
+
+nums = [1, 2]
+val = 6
+double(nums, val)
+
+# Class
+class MyClass:
+    # Constructor
+    def __init__(self, nums): 
+        # Create member variables
+        self.nums = nums
+        self.size = len(nums)
+
+    # self key word required as param
+    def getLength(self):
+        return self.size
+    
+    def getDoubleLength(self):
+        return 2 * self.getLength()
+nums = [1, 2, 3, 4]
+# TODO figure out why Line 428 gives "AttributeError: 'list' object has no attribute 'size'" error
+#print(MyClass.getLength(nums))
+
+
+
